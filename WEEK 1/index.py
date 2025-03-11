26518d3a7e55b2d4b36afd41f1d63b52343902ac
Bkibiwot73 <bkibiwot73@gmail.com>
2025-03-11 11:48:31 +0300

    git commit -m "data types"

--- WEEK 1/index.py
+++ WEEK 1/index.py
@@ -1,1 +1,18 @@
 print("Hello world")
+
+number = 10
+print(number)
+
+name = "Brian Kibet" #string data type
+age = 26 #int data type
+width = 5.5 #float data type
+
+print(name)
+print(width)
+
+
+
+#List DataTypes
+languages = ["python", "HTML", "CSS", 3, 5.7, True]
+#PRINT (LANGUAGES)
+print(languages[-4])
