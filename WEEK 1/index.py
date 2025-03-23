0220904d9cfc37f3190bb934c3a324564512d98e
Bkibiwot73 <bkibiwot73@gmail.com>
2025-03-23 13:30:25 +0300

    git commit -m "Add new feature"

--- WEEK 1/index.py
+++ WEEK 1/index.py
@@ -1,3 +1,24 @@
-name = input("What's your name?")
-color = input("What's your favorite color?")
-print("Hello " + name + " your favorite color is " + color)
+# Basic calculator
+
+# Ask the user to input the first number
+num1 = float(input("Enter first number: "))
+
+# Ask the user to input the second number
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator: (+, -, *, /): ")
+
+# Check the operator and perform the operation
+
+if operator == "+":
+    print("The sum of the two numbers is: ", num1 + num2)
+elif operator == "-":
+    print("The difference of the two numbers is: ", num1 - num2)
+elif operator == "*":
+    print("The product of the two numbers is: ", num1 * num2)
+elif operator == "/":
+    if num2 != 0:
+        print("The division of the two numbers is: ", num1 / num2)
+    else: print("Division by zero is not allowed.")
+else:
+    print("Invalid operator. Please enter a valid operator.")
+
