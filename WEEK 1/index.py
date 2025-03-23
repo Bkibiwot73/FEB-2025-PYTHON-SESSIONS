68a033a344b081928402c8c981ab957a7c37c46f
Bkibiwot73 <bkibiwot73@gmail.com>
2025-03-23 12:37:18 +0300

    git commit -m "Week 1"

--- WEEK 1/index.py
+++ WEEK 1/index.py
@@ -1,18 +1,4 @@
-print("Hello world")
-
-number = 10
-print(number)
-
-name = "Brian Kibet" #string data type
-age = 26 #int data type
-width = 5.5 #float data type
-
-print(name)
-print(width)
-
-
-
-#List DataTypes
-languages = ["python", "HTML", "CSS", 3, 5.7, True]
-#PRINT (LANGUAGES)
-print(languages[-4])
+name = input("What's your name?")
+color = input("What's your favorite color?")
+print(name + " likes " + color)
+print("Hello " + name + " your favorite color is " + color)
